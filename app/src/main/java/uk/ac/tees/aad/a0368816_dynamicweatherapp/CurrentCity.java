45f9a6ac8d9dd7be733012ef88cc208ad088b18f
clemsmith <clemzysmith@gmail.com>
2022-08-07 10:25:32 +0100

    Front end - current city design

---
+++ app/src/main/java/uk/ac/tees/aad/a0368816_dynamicweatherapp/CurrentCity.java
@@ -0,0 +1,15 @@
+package uk.ac.tees.aad.a0368816_dynamicweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentCity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_city);
+
+    }
+}
