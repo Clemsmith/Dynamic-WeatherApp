c5ae319e340ce3d20629357746d5ffee8a0b4aef
clemsmith <clemzysmith@gmail.com>
2022-08-07 10:06:24 +0100

    Initial Commit

---
+++ app/src/main/java/uk/ac/tees/aad/a0368816_dynamicweatherapp/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.aad.a0368816_dynamicweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
